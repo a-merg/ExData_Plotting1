a9d688b6bcb5de6e12e3423cd1629e7903ebee6c
Alex Merg <35757302+a-merg@users.noreply.github.com>
2019-02-19 22:47:00 -0600

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,38 @@
+## Read in data
+
+PowerData <- read.table("./household_power_consumption.txt", 
+                        header = TRUE, 
+                        sep = ";", 
+                        na.strings = "?")
+
+## Subset for designated rows
+
+PlotData <- subset(PowerData, Date %in% c("1/2/2007", "2/2/2007"))
+
+## Set Date column to date format
+
+PlotData$Date <- as.Date(PlotData$Date, format = "%d/%m/%Y")
+
+## Concatenate Date and Time columns and store in new column
+
+PastedTime <- paste(PlotData$Date, PlotData$Time, sep = " ")
+PlotData$DateAndTime <- as.POSIXct(PastedTime)
+
+## Run plot
+
+png("plot4.png")
+par(mfrow = c(2,2))
+with(PlotData, plot(Global_active_power ~ DateAndTime, 
+                    type = "l", 
+                    xlab = "", 
+                    ylab = "Global Active Power"))
+with(PlotData, plot(Voltage ~ DateAndTime, type = "l"))
+with(PlotData, plot(Sub_metering_1 ~ DateAndTime, type = "n", ylab = "Energy sub metering", xlab = ""))
+with(PlotData, points(Sub_metering_2 ~ DateAndTime, col = "red", type = "l"))
+with(PlotData, points(Sub_metering_1 ~ DateAndTime, col = "black", type = "l"))
+with(PlotData, points(Sub_metering_3 ~ DateAndTime, col = "blue", type = "l"))
+legend("topright", lty =1,
+                   col = c("black", "red", "blue"),
+                   legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(PlotData, plot(Global_reactive_power ~ DateAndTime, type = "l"))
+dev.off()
